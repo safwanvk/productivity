f0949824de2762b1309832b5eeb28d526bc56201
safwanvk <safwanvalakundil@gmail.com>
2021-02-25 13:01:42 +0530

    :sparkles: Setup model User

--- ProductivityApp/urls.py
+++ ProductivityApp/urls.py
@@ -20,3 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    fb_profile = models.CharField(max_length=255)
+    twitter_profile = models.CharField(max_length=255)
+    linkedin_profile = models.CharField(max_length=255)
+    website = models.CharField(max_length=255)
+    objects = models.Manager()
