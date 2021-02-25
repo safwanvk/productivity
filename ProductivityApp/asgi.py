10eda7d748f4fc3b88b3d3ba1afeb0a6a7808379
safwanvk <safwanvalakundil@gmail.com>
2021-02-25 11:35:43 +0530

    :tada: Initial commit

---
+++ ProductivityApp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ProductivityApp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProductivityApp.settings')
+
+application = get_asgi_application()
