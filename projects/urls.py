8fc50823d57e5c49bc12c77f20394c24c96cfcd4
safwanvk <safwanvalakundil@gmail.com>
2021-02-25 22:09:53 +0530

    :sparkles: Setup ProjectViewSet

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+# Routers provide an easy way of automatically determining the URL conf.
+from rest_framework import routers
+from . views import ProjectViewset
+
+router = routers.DefaultRouter()
+router.register(r'', ProjectViewset, basename='project')
+
+app_name = 'projects'
 
 urlpatterns = [
-
+    path('', include(router.urls))
+    
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
 
 # Create your views here.
+# ViewSets define the view behavior.
+from rest_framework import viewsets, generics
+from rest_framework.generics import get_object_or_404
+
+from rest_framework.response import Response
+
+from datetime import datetime, timedelta
+from .models import Project
+
+# Employee Viewset
+from .serializer import ProjectSerializer
+
+
+class ProjectViewset(viewsets.ViewSet):
+
+    def create(self, request):
+        try:
+            serializer = ProjectSerializer(data=request.data, context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"error": False, "message": "Project Data Save Successfully"})
+        except:
+            return Response({"error": True, "message": "Error During Saving Project Data"})
+
+
+    def list(self, request):
+        try:
+            project = Project.objects.all()
+            serializer = ProjectSerializer(project, many=True, context={"request": request})
+            return Response({"error": False, "message": "All Project List Data", "data": serializer.data})
+
+        except:
+            return Response({"error": True, "message": "Error During Project List Data"})
+
+
+    def retrieve(self, request, pk=None):
+        try:
+            queryset = Project.objects.all()
+            project = get_object_or_404(queryset, pk=pk)
+            serializer = ProjectSerializer(project, context={"request": request})
+            return Response({"error": False, "message": "Single Data Fetch", "data": serializer.data})
+        
+        except:
+            return Response({"error": True, "message": "Error During Project Single Data Fetch"})
+
+    def update(self, request, pk=None):
+        try:
+            queryset = Project.objects.all()
+            project = get_object_or_404(queryset, pk=pk)
+            serializer = ProjectSerializer(project, data=request.data, context={"request": request})
+            serializer.is_valid()
+            serializer.save()
+            return Response({"error": False, "message": "Data Has Been Updated"})
+        
+        except:
+            return Response({"error": True, "message": "Error During Data Has Been Updated"})
+
+    def destroy(self, request, pk=None):
+        try:
+            queryset = Project.objects.all()
+            project = get_object_or_404(queryset, pk=pk)
+            Project.delete()
+            return Response({"error": False, "message": "Data Has Been Deleted"})
+        
+        except:
+            return Response({"error": True, "message": "Error During Data Has Been Deleted"})
